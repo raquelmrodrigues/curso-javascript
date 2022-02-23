0d655315f0661619643a5535ad8250ab0065fd44
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-02-23 12:11:54 -0300

    Update main.js

--- aula-15/main.js
+++ aula-15/main.js
@@ -5,10 +5,10 @@ const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Seu número -2 é ${numero - 2}</p>`;
-texto.innerHTML = `<p>A raiz quadrada do seu numero é ${numero * 0.5}</p>`;
-texto.innerHTML = `<p>Seu número é inteiro ${Number.isInteger(numero)}</p>`;
-texto.innerHTML = `<p>Seu número é NaN ${Number.isNaN(numero)}</p>`;
-texto.innerHTML = `<p>Seu número arredondado para baixo é ${Math.floor(numero)}</p>`;
-texto.innerHTML = `<p>Seu número arredondado para cima é ${Math.ceil(numero)}</p>`;
-texto.innerHTML = `<p>Seu número com duas casas ${numero.toFixed(2)}</p>`;
+texto.innerHTML += `<p>Seu número -2 é ${numero - 2}</p>`;
+texto.innerHTML += `<p>A raiz quadrada do seu numero é ${numero * 0.5}</p>`;
+texto.innerHTML += `<p>Seu número é inteiro ${Number.isInteger(numero)}</p>`;
+texto.innerHTML += `<p>Seu número é NaN ${Number.isNaN(numero)}</p>`;
+texto.innerHTML += `<p>Seu número arredondado para baixo é ${Math.floor(numero)}</p>`;
+texto.innerHTML += `<p>Seu número arredondado para cima é ${Math.ceil(numero)}</p>`;
+texto.innerHTML += `<p>Seu número com duas casas ${numero.toFixed(2)}</p>`;
