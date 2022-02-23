51029813d5027a4bc6598bdc215710d0c25f62a0
Raquel Rodrigues <raquel.mr.rodrigues@gmail.com>
2022-02-23 09:55:29 -0300

    aula 38

---
+++ aula-38/main.js
@@ -0,0 +1,9 @@
+const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#FFFFFF';
+}
